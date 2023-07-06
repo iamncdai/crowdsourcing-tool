993ecd104c7f68fd4da73f2c17fc3d35f4498e20
Tam <ntam597.van@gmail.com>
2023-07-06 21:44:50 +0700

    update 1

--- core-service/models/PhanCongGanNhan.py
+++ core-service/models/PhanCongGanNhan.py
@@ -1,12 +1,12 @@
 from .Database import db
 class PhanCongGanNhan(db.Model):
     __tablename__ = 'PhanCongGanNhan'
-    idDuAn = db.Column(db.Integer, primary_key=True)
+    idDuLieu = db.Column(db.String(100), primary_key=True)
     idNguoiGanNhan = db.Column(db.Integer, primary_key=True)
     TrangThai = db.Column(db.String(30))
 
-    def __init__(self, idDuAn, idNguoiGanNhan,TrangThai):
-        self.idDuAn = idDuAn
+    def __init__(self, idDuLieu, idNguoiGanNhan,TrangThai):
+        self.idDuLieu = idDuLieu
         self.idNguoiGanNhan = idNguoiGanNhan
         self.TrangThai = TrangThai
         
